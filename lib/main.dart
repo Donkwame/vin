ce90f5660564390e96e184cd910553f5191e1687
Prince Turkson <nanakwameadu205@gmail.com>
2023-12-06 16:06:50 +0000

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+// main.dart
+
+import 'package:flutter/material.dart';
+import 'vin_decoder_widget.dart'; // Replace with the correct import path
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Vin Decoder'),
+        ),
+        body: const VinDecoderWidget(),
+      ),
+    );
+  }
+}
